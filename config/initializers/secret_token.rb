386984ac629e75a8d0fad862698976609a0a6fc5
user <user@appcara-dev1.(none)>
2012-01-30 12:10:56 +0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorturl::Application.config.secret_token = 'c16d7537f267bc92c9c4ad78516fdcd7584e6daa3fab33a1b63e64ca62ddf4a3ed3e1cf554c94e39fd7a039a94681c2f4f9efc9e39e24751de7699f85e510790'
